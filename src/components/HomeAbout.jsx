f4576a0d9cbfb78fa3abb0fc5eff02343001b621
ogungbile-oluwasogo <ogungbileshogo@gmail.com>
2023-08-08 20:29:49 +0100

    adjustment on some mobile view in some component

--- src/components/HomeAbout.jsx
+++ src/components/HomeAbout.jsx
@@ -4,19 +4,14 @@ const HomeAbout = ({ abouts }) => {
   return (
     <div className={style["home-about-container"]}>
       <div className={style["about-flex-one"]}>
-        <h3>CONECTAR</h3>
+        <h3>OUR MISSION</h3>
         <p>
-          In the modern world, homeowners often find themselves in need of
-          various services for their homes, ranging from plumbing and electrical
-          work to home repairs and maintenance. On the other hand, numerous
-          skilled professionals and service providers excel in these fields,
-          offering their expertise and assistance. However, connecting these two
-          parties efficiently and ensuring a seamless experience can be
-          challenging. This is where a CONECTAR, plays a crucial role as a
-          facilitator and coordinator.By bridging the gap between home service
-          providers and homeowners, We are instrumental in creating a harmonious
-          and efficient ecosystem.
+          Our mission is to create a thriving community where service providers
+          can showcase their expertise and recipients can easily find and engage
+          with the perfect service for their needs. <br />
+          <a href="/">Read More...</a>
         </p>
+
         <h4>We: </h4>
         <ul>
           <li>
